dbd0aba48dc2802d3064619435f804b85064ede5
Ugur Emre Dogan <ue.dogan@gmail.com>
2024-04-08 20:33:45 -0700

    Updated the paths

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("transactions.urls")),
     path("api/", include("transactions_api.urls")),
-    # path("api-auth/", include("rest_framework.urls")),
+    path("api-auth/", include("rest_framework.urls")),
 ]
